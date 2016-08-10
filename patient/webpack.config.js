cdb91003cd0ac9e46b0c47b0e1645c554a936776
Ino de Bruijn <ino@ino.pm>
2016-08-10 12:48:26 -0400

    Add production file to dist/

--- patient/webpack.config.js
+++ patient/webpack.config.js
@@ -1,20 +1,22 @@
 var path = require('path')
 var webpack = require('webpack')
 
-module.exports = {
+var env = process.env.NODE_ENV
+var config = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-hot-middleware/client',
-    './clinical'
+    './clinical_table'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'clinical_table.js',
     publicPath: '/static/'
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
   ],
   module: {
     loaders: [
@@ -32,3 +34,19 @@ module.exports = {
     ]
   }
 }
+
+if (env === 'production') {
+  config.devtool = 'source-map';
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: {
+        except: ['$super', '$', 'exports', 'require']
+      }
+    })
+  )
+} else {
+  config.entry.push('webpack-hot-middleware/client')
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+
+module.exports = config
